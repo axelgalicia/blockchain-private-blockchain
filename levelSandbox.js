c29469a86154cde1f3a1b35493720988eb9a597d
Axel Galicia <axelgalicia@gmail.com>
2018-11-11 21:33:41 -0500

    Using promises and changing test to print values of chain

--- levelSandbox.js
+++ levelSandbox.js
@@ -3,52 +3,102 @@
 |  =============================================================*/
 
 const level = require('level');
-const chainDB = './chaindata';
-const db = level(chainDB);
 
-// Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
-  })
-}
+class Storage {
 
-// Get data from levelDB with key
-function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
-}
+  constructor(dataLocation) {
+    this.db = level(dataLocation);
+  }
+  // Add data to levelDB with key/value pair
+  addLevelDBData(key, value) {
+    this.db.put(key, value, function (err) {
+      if (err) return console.log('Block ' + key + ' submission failed', err);
+    });
+  }
 
-// Add data to levelDB with value
-function addDataToLevelDB(value) {
+  // Get data from levelDB with key
+  getLevelDBData(key) {
+    this.db.get(key, function (err, value) {
+      if (err) return console.log('Not found!', err);
+      console.log('Value = ' + value);
+    })
+  }
+
+  // Add data to levelDB with value
+  async addDataToLevelDB(value) {
+    let self = this;
     let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+    return new Promise((resolve, reject) => {
+      this.db.createReadStream().on('data', function (data) {
+        i++;
+      }).on('error', function (err) {
+        console.log('Unable to read data stream!', err);
+        reject(err);
+      }).on('close', function () {
+        console.log('Block #' + i);
+        self.addLevelDBData(i, value);
+        resolve(value);
+      });
+
+    });
+
+  }
+
+
+  // Prints all data in the db
+  async printAllData() {
+    let self = this;
+    let i = 0;
+    return new Promise((resolve, reject) => {
+      this.db.createReadStream().on('data', function (data) {
+        console.log(data);
+        i++;
+      }).on('error', function (err) {
+        console.log('Unable to read data stream!', err);
+        reject(err);
+      }).on('close', function () {
+        resolve(true);
+      });
+
+    });
+
+  }
+
+
 }
 
-/* ===== Testing ==============================================================|
-|  - Self-invoking function to add blocks to chain                             |
-|  - Learn more:                                                               |
-|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
-|                                                                              |
-|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
-|     (13.89 hours for 500,000 blocks)                                         |
-|    Bitcoin blockchain adds 8640 blocks per day                               |
-|     ( new block every 10 minutes )                                           |
-|  ===========================================================================*/
 
+/* ===== Test class for Storage ===================================*/
+class TestStorage {
 
-(function theLoop (i) {
-  setTimeout(function () {
-    addDataToLevelDB('Testing data');
-    if (--i) theLoop(i);
-  }, 100);
+  constructor(locationTest) {
+    this.db = new Storage(locationTest);
+  }
+
+  //Insert i items into the storage using i as a key
+  async insert(i) {
+    let self = this;
+    for (let z = 0; z < i; z++) {
+      let promise = await self.db.addDataToLevelDB(`Testing data ${z}`);
+      console.log(promise);
+    }
+  }
+  //Reads all the storage db and print all the values
+  async printAll() {
+    let self = this;
+    let promise = await self.db.printAllData();
+  }
+}
+
+//IIFE that inserts i elements and then print them
+(async function (i) {
+  let testStorage = new TestStorage('./testchain');
+  await testStorage.insert(i);
+  console.log('Printing chain:')
+  testStorage.printAll();
 })(10);
+
+
+module.exports = {
+  Storage
+}
